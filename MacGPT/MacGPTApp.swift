a30df2b7901c1000d800672e65f8853c64963b62
Kevin Peng <pengkevi@amazon.com>
2023-03-21 16:57:18 -0400

    Initial Commit

---
+++ MacGPT/MacGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacGPTApp.swift
+//  MacGPT
+//
+//  Created by Peng, Kevin [C] on 2023-03-21.
+//
+
+import SwiftUI
+
+@main
+struct MacGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
